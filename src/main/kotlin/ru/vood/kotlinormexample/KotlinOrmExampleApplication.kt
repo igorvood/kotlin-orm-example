324379358282a2689e43cf046dd364a7872c9afd
vood <balitsky_igor@list.ru>
2022-11-03 10:23:13 +0300

    Initial commit

---
+++ src/main/kotlin/ru/vood/kotlinormexample/KotlinOrmExampleApplication.kt
@@ -0,0 +1,11 @@
+package ru.vood.kotlinormexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinOrmExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinOrmExampleApplication>(*args)
+}
---
+++ src/test/kotlin/ru/vood/kotlinormexample/KotlinOrmExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.vood.kotlinormexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinOrmExampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
